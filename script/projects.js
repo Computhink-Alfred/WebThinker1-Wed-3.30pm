2729e559dc56f709af164afbf0392a8f3b0e6c40
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-10-30 08:53:18 +0000

    20241030

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,52 @@
-// 
+let x, y, dx, dy, canvas, ctx, ballRadius;
+
+window.onload = () => {
+    canvas = document.getElementById('canvasBoard');
+    ctx = canvas.getContext('2d');
+
+    ballRadius = 10;
+
+    x = canvas.width / 2 - ballRadius / 2;
+    y = canvas.height / 2 - ballRadius / 2;
+    dx = 2;
+    dy = 2;
+
+    updateCanvas();
+}
+
+// Erase previous frame and draw new frame
+const updateCanvas = () => {
+    // Erase canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the ball
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x = x + dx;
+    y = y + dy;
+
+    requestAnimationFrame(updateCanvas);
+}
+
+// requestAnimationFrame(updateCanvas);
+
+// // check collision with the left or right edge
+// if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+//     dx = -dx;
+// }
+// // check collision with the top or bottom edge
+// if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+//     dy = -dy;
+// }
